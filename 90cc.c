572adf5b1346820801a6a797ef8b60a12422736d
kuked <kedoku@gmail.com>
2021-03-01 19:26:04 +0900

    単項プラスと単項マイナス

--- 90cc.c
+++ 90cc.c
@@ -42,6 +42,7 @@ Token* token;
 
 Node* expr(void);
 Node* mul(void);
+Node* unary(void);
 Node* primary(void);
 
 void error(char* fmt, ...) {
@@ -153,14 +154,22 @@ Node* primary(void) {
   return new_node_num(expect_number());
 }
 
+Node* unary(void) {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), unary());
+  return primary();
+}
+
 Node* mul(void) {
-  Node* node = primary();
+  Node* node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
