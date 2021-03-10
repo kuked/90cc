636ebd75d0ae7cf5a1440dc12a5f3187dbba0e97
kuked <kedoku@gmail.com>
2021-03-10 18:57:54 +0900

    cosmetic.

--- parse.c
+++ parse.c
@@ -28,7 +28,6 @@ void error_at(char* loc, char* fmt, ...) {
 bool consume(char *op) {
     if ((token->kind != TK_RESERVED && token->kind != TK_RETURN) ||
         (int)strlen(op) != token->len || memcmp(token->str, op, token->len))
-        //if (token->kind != TK_RESERVED || (int)strlen(op) != token->len || memcmp(token->str, op, token->len))
         return false;
     token = token->next;
     return true;
